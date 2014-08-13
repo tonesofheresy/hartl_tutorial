5f91c776b03abbdbffc78908a9306d718cae5cea
Sunny Mittal <86.s.mittal@gmail.com>
2014-08-13 08:44:26 -0600

    Finish Ch5 Exercises

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -9,7 +9,7 @@ describe "User pages" do
 		before { visit signup_path }
 
 		it { should have_content('Sign up') }
-		it { should have_title('Sign up') }
+		it { should have_title('Sign Up') }
 		
 	end
 
